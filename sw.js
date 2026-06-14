269711189af7b90225525b9760530dc0684b8dfd
KIOS DUBBING <gudangdubbing@gmail.com>
2026-06-14 23:48:44 +0700

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "blogger-pwa-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
