9bb281613e35c8d0ab6d95f34d93a72090ad45dd
KIOS DUBBING <gudangdubbing@gmail.com>
2026-06-22 23:14:08 +0700

    Update service worker cache name and fetch logic

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
-const CACHE_NAME = "blogger-pwa-v1";
+const CACHE_NAME = "kios-dubbing-v1";
+
+const urlsToCache = [
+"/",
+"/offline.html"
+];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
+event.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.filter(key=>key!==CACHE_NAME)
+.map(key=>caches.delete(key))
+);
+})
+);
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request).then(networkResponse => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-    })
-  );
+event.respondWith(
+fetch(event.request)
+.catch(()=>{
+return caches.match(event.request)
+.then(response=>{
+return response || caches.match("/offline.html");
+});
+})
+);
 });
